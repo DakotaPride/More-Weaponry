f4cd4abfb985779bca2f49c66338858ea84e0230
DakotaPride <rjtube13@gmail.com>
2022-01-18 07:33:29 -0600

    More Weaponry 1.18

--- src/main/java/net/DakotaPride/moreweaponry/item/ElderScaleTridentItem.java
+++ src/main/java/net/DakotaPride/moreweaponry/item/ElderScaleTridentItem.java
@@ -2,8 +2,11 @@ package net.DakotaPride.moreweaponry.item;
 
 import net.minecraft.item.TridentItem;
 
+
+
 public class ElderScaleTridentItem extends TridentItem {
     public ElderScaleTridentItem(Settings settings) {
         super(settings);
     }
-}
+
+    }
