48eef6fff568eb077d99919e9046ac027289e629
DakotaPride <rjtube13@gmail.com>
2022-01-18 10:35:52 -0600

    More Weaponry 1.18

--- src/main/java/net/DakotaPride/moreweaponry/mixin/MoreWeaponryMixin.java
+++ src/main/java/net/DakotaPride/moreweaponry/mixin/MoreWeaponryMixin.java
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class MoreWeaponryMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		MoreWeaponry.LOGGER.info("This line is printed by an example mod mixin!");
+		MoreWeaponry.LOGGER.info("This line is printed by More Weaponry mixin!");
 	}
 }
