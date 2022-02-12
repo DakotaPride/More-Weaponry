152adc3794744126ed67e1ece24573b5b2722b10
DakotaPride <rjtube13@gmail.com>
2022-02-12 13:14:07 -0600

    Adding button to Development page for Midas Foods, an upcoming mod I'm working on

--- src/main/java/net/DakotaPride/moreweaponry/mixin/TitleScreenMixin.java
+++ src/main/java/net/DakotaPride/moreweaponry/mixin/TitleScreenMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,8 +24,8 @@ public abstract class TitleScreenMixin extends Screen {
     @Inject(at = @At("RETURN"), method = "initWidgetsNormal")
         private void customButtonMod(int y, int spacingY, CallbackInfo ci) {
         this.addDrawableChild(new ButtonWidget(this.width /
-                2 + 104, y, 20, 20, new LiteralText("MF"), (button) -> {
-            this.client.setScreen(new SelectWorldScreen(this));
+                2 + 407, y + -180 + 12, 70, 20, new LiteralText("Midas Foods"), (button) -> {
+            Util.getOperatingSystem().open("https://github.com/DakotaPride/Midas-Foods-1.18");;
         }));
     }
 
