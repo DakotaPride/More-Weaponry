2c635dd320fd4c645d9c60d1f23df470593049bd
DakotaPride <rjtube13@gmail.com>
2022-02-15 16:42:30 -0600

    Adding Mining Levels

---
+++ src/main/java/net/DakotaPride/moreweaponry/item/MoreWeaponryMiningLevels.java
@@ -0,0 +1,18 @@
+package net.DakotaPride.moreweaponry.item;
+
+public class MoreWeaponryMiningLevels {
+
+    public static final int CIRTICT = 6;
+
+    public static final int COOLED_CIRTICT = 7;
+
+    public static final int HEATED_CIRTICT = 7;
+
+    public static final int GUARDIAN_SCALE = 5;
+
+    public static final int ELDER_GUARDIAN_SCALE = 5;
+
+    private MoreWeaponryMiningLevels() {
+    }
+
+}
