c43bfcea23fe4f02c952615f4080b8d6c64b5d65
DakotaPride <rjtube13@gmail.com>
2022-03-15 19:05:48 -0500

    Cirtict Forge Added

---
+++ src/main/java/net/DakotaPride/moreweaponry/block/entity/CirtictForgeBlock.java
@@ -0,0 +1,68 @@
+package net.DakotaPride.moreweaponry.block.entity;
+
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class CirtictForgeBlock extends BlockWithEntity implements BlockEntityProvider {
+    public CirtictForgeBlock(Settings settings) {
+        super(settings);
+    }
+
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof CirtictForgeEntity) {
+                ItemScatterer.spawn(world, pos, (CirtictForgeEntity)blockEntity);
+                world.updateComparators(pos,this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos,
+                              PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
+
+            if (screenHandlerFactory != null) {
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new CirtictForgeEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, MoreWeaponryBlockEntities.CIRTICT_FORGE, CirtictForgeEntity::tick);
+    }
+}
---
+++ src/main/java/net/DakotaPride/moreweaponry/block/entity/CirtictForgeEntity.java
@@ -0,0 +1,160 @@
+package net.DakotaPride.moreweaponry.block.entity;
+
+import net.DakotaPride.moreweaponry.item.inventory.ImplementedInventory;
+import net.DakotaPride.moreweaponry.recipe.CirtictForgeRecipe;
+import net.DakotaPride.moreweaponry.recipe.CoreForgeRecipe;
+import net.DakotaPride.moreweaponry.screen.CirtictForgeScreenHandler;
+import net.DakotaPride.moreweaponry.screen.CoreForgeScreenHandler;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class CirtictForgeEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
+    private final DefaultedList<ItemStack> inventory =
+            DefaultedList.ofSize(4, ItemStack.EMPTY);
+
+
+    protected final PropertyDelegate propertyDelegate;
+    private int progress = 0;
+    private int maxProgress = 72;
+    private int fuelTime = 0;
+    private int maxFuelTime = 0;
+
+    public CirtictForgeEntity(BlockPos pos, BlockState state) {
+        super(MoreWeaponryBlockEntities.CIRTICT_FORGE, pos, state);
+        this.propertyDelegate = new PropertyDelegate() {
+            public int get(int index) {
+                switch (index) {
+                    case 0: return CirtictForgeEntity.this.progress;
+                    case 1: return CirtictForgeEntity.this.maxProgress;
+                    case 2: return CirtictForgeEntity.this.fuelTime;
+                    case 3: return CirtictForgeEntity.this.maxFuelTime;
+                    default: return 0;
+                }
+            }
+
+            public void set(int index, int value) {
+                switch(index) {
+                    case 0: CirtictForgeEntity.this.progress = value; break;
+                    case 1: CirtictForgeEntity.this.maxProgress = value; break;
+                    case 2: CirtictForgeEntity.this.fuelTime = value; break;
+                    case 3: CirtictForgeEntity.this.maxFuelTime = value; break;
+                }
+            }
+
+            public int size() {
+                return 4;
+            }
+        };
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return inventory;
+    }
+
+    @Override
+    public Text getDisplayName() {
+        return new LiteralText("Cirtict Forge");
+    }
+
+    @Nullable
+    @Override
+    public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
+        return new CirtictForgeScreenHandler(syncId, inv, this, this.propertyDelegate);
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        Inventories.writeNbt(nbt, inventory);
+        nbt.putInt("cirtict_forge.progress", progress);
+        nbt.putInt("cirtict_forge.fuelTime", fuelTime);
+        nbt.putInt("cirtict_forge.maxFuelTime", maxFuelTime);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        Inventories.readNbt(nbt, inventory);
+        super.readNbt(nbt);
+        progress = nbt.getInt("cirtict_forge.progress");
+        fuelTime = nbt.getInt("cirtict_forge.fuelTime");
+        maxFuelTime = nbt.getInt("cirtict_forge.maxFuelTime");
+    }
+
+    public static void tick(World world, BlockPos pos, BlockState state, CirtictForgeEntity entity) {
+            if(hasRecipe(entity)) {
+                entity.progress++;
+                if(entity.progress > entity.maxProgress) {
+                    craftItem(entity);
+                }
+            }
+        else {
+            entity.resetProgress();
+        }
+    }
+
+    private static boolean hasRecipe(CirtictForgeEntity entity) {
+        World world = entity.world;
+        SimpleInventory inventory = new SimpleInventory(entity.inventory.size());
+        for (int i = 0; i < entity.inventory.size(); i++) {
+            inventory.setStack(i, entity.getStack(i));
+        }
+
+        Optional<CirtictForgeRecipe> match = world.getRecipeManager()
+                .getFirstMatch(CirtictForgeRecipe.Type.INSTANCE, inventory, world);
+
+        return match.isPresent() && canInsertAmountIntoOutputSlot(inventory)
+                && canInsertItemIntoOutputSlot(inventory, match.get().getOutput());
+    }
+
+    private static void craftItem(CirtictForgeEntity entity) {
+        World world = entity.world;
+        SimpleInventory inventory = new SimpleInventory(entity.inventory.size());
+        for (int i = 0; i < entity.inventory.size(); i++) {
+            inventory.setStack(i, entity.getStack(i));
+        }
+
+        Optional<CirtictForgeRecipe> match = world.getRecipeManager()
+                .getFirstMatch(CirtictForgeRecipe.Type.INSTANCE, inventory, world);
+
+        if(match.isPresent()) {
+            entity.removeStack(1,1);
+            entity.removeStack(2,1);
+
+            entity.setStack(3, new ItemStack(match.get().getOutput().getItem(),
+                    entity.getStack(3).getCount() + 1));
+
+            entity.resetProgress();
+        }
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleInventory inventory, ItemStack output) {
+        return inventory.getStack(3).getItem() == output.getItem() || inventory.getStack(3).isEmpty();
+    }
+
+    private static boolean canInsertAmountIntoOutputSlot(SimpleInventory inventory) {
+        return inventory.getStack(3).getMaxCount() > inventory.getStack(3).getCount();
+    }
+
+}
