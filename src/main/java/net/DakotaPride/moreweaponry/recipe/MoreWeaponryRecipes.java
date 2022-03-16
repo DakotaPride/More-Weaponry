5bef6fc1ebf4a991e47bf407893308507d8f8afb
DakotaPride <rjtube13@gmail.com>
2022-03-15 19:11:35 -0500

    Cirtict Forge Recipes

--- src/main/java/net/DakotaPride/moreweaponry/recipe/MoreWeaponryRecipes.java
+++ src/main/java/net/DakotaPride/moreweaponry/recipe/MoreWeaponryRecipes.java
@@ -16,5 +16,10 @@ public class MoreWeaponryRecipes {
                 EssenceTranslatorRecipe.Serializer.INSTANCE);
         Registry.register(Registry.RECIPE_TYPE, new Identifier(MoreWeaponry.MOD_ID, EssenceTranslatorRecipe.Type.ID),
                 EssenceTranslatorRecipe.Type.INSTANCE);
+        // Cirtict Forge
+        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MoreWeaponry.MOD_ID, CirtictForgeRecipe.Serializer.ID),
+                CirtictForgeRecipe.Serializer.INSTANCE);
+        Registry.register(Registry.RECIPE_TYPE, new Identifier(MoreWeaponry.MOD_ID, CirtictForgeRecipe.Type.ID),
+                CirtictForgeRecipe.Type.INSTANCE);
     }
 }
