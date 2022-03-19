b5f9ba59d59afd2b10a5081e81c327e440a5fff2
DakotaPride <rjtube13@gmail.com>
2022-03-19 13:27:37 -0500

    Updating To 1.18.2 (BETA)

--- src/main/java/net/DakotaPride/moreweaponry/MoreWeaponry.java
+++ src/main/java/net/DakotaPride/moreweaponry/MoreWeaponry.java
@@ -52,6 +52,9 @@ public class MoreWeaponry implements ModInitializer {
 
 		MoreWeaponryLootTableModifiers.modifyLootTables();
 
+		MoreWeaponryTags.MoreWeaponryBlockTags();
+		MoreWeaponryTags.MoreWeaponryItemTags();
+
 		GeckoLib.initialize();
 
 	}
--- src/main/java/net/DakotaPride/moreweaponry/util/MoreWeaponryTags.java
+++ src/main/java/net/DakotaPride/moreweaponry/util/MoreWeaponryTags.java
@@ -7,7 +7,29 @@ import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import static net.DakotaPride.moreweaponry.util.MoreWeaponryTags.Blocks.*;
+import static net.DakotaPride.moreweaponry.util.MoreWeaponryTags.Items.*;
+
 public class MoreWeaponryTags {
+
+    public static void MoreWeaponryBlockTags() {
+        TagKey<Block> sand_replaceable_overworld = SAND_REPLACEABLE_OVERWORLD;
+        TagKey<Block> dripstone_replaceable = DRIPSTONE_REPLACEABLE_OVERWORLD;
+        TagKey<Block> peplex_replaceable = PEPLEX_REPLACEABLE;
+        TagKey<Block> intoxicated_plants = INTOXICATED_PLANTS;
+        TagKey<Block> small_intoxicated_flowers = SMALL_INTOXICATED_FLOWERS;
+    }
+
+    public static void MoreWeaponryItemTags() {
+        TagKey<Item> battleaxes = BATTLEAXES;
+        TagKey<Item> essence = ESSENCE;
+        TagKey<Item> handles = HANDLES;
+        TagKey<Item> rapiers = RAPIERS;
+        TagKey<Item> shields = SHIELDS;
+        TagKey<Item> bluestone = BLUESTONE;
+        TagKey<Item> cobbled_bluestone = COBBLED_BLUESTONE;
+        TagKey<Item> cobbled_limestone = COBBLED_LIMESTONE;
+    }
 
     public static class Blocks {
         public static final TagKey<Block> SAND_REPLACEABLE_OVERWORLD =
@@ -24,10 +46,6 @@ public class MoreWeaponryTags {
         private static TagKey<Block> createTag(String name) {
             return TagKey.of(Registry.BLOCK_KEY, new Identifier(MoreWeaponry.MOD_ID, name));
         }
-
-        private static TagKey<Block> createCommonTag(String name) {
-            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
-        }
     }
 
     public static class Items {
@@ -40,16 +58,8 @@ public class MoreWeaponryTags {
         public static final TagKey<Item> COBBLED_BLUESTONE = createCommonTag("cobbled_bluestone");
         public static final TagKey<Item> COBBLED_LIMESTONE = createCommonTag("cobbled_limestone");
 
-
-        private static TagKey<Item> createTag(String name) {
-            return TagKey.of(Registry.ITEM_KEY, new Identifier(MoreWeaponry.MOD_ID, name));
-        }
-
         private static TagKey<Item> createCommonTag(String name) {
             return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
-        }
-
-        private static class Identified<T> {
         }
     }
 
