212f377e642a29ddf78f68151fef4232824c17e0
DakotaPride <rjtube13@gmail.com>
2022-04-09 16:43:08 -0500

    v1.18.8

--- src/main/java/net/DakotaPride/moreweaponry/effect/status_effects/base/CelestialEffect.java
+++ src/main/java/net/DakotaPride/moreweaponry/effect/status_effects/base/CelestialEffect.java
@@ -1,24 +1,13 @@
 package net.DakotaPride.moreweaponry.effect.status_effects.base;
 
-import net.DakotaPride.moreweaponry.effect.MoreWeaponryEffects;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
-import net.minecraft.entity.effect.HealthBoostStatusEffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffects;
 
-public class CelestialEffect extends HealthBoostStatusEffect {
+public class CelestialEffect extends StatusEffect {
     public CelestialEffect(StatusEffectCategory statusEffectCategory, int i) {
         super(statusEffectCategory, i);
-    }
-
-
-    @Override
-    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity.hasStatusEffect(MoreWeaponryEffects.CELESTIAL)) {
-            entity.removeStatusEffect(StatusEffects.REGENERATION);
-        }
     }
 
     public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
---
+++ src/main/java/net/DakotaPride/moreweaponry/mixin/StatusEffectMixin.java
@@ -0,0 +1,22 @@
+package net.DakotaPride.moreweaponry.mixin;
+
+import net.DakotaPride.moreweaponry.effect.MoreWeaponryEffects;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(StatusEffect.class)
+public class StatusEffectMixin {
+    @Inject(method = "applyUpdateEffect", at = @At("HEAD"))
+    public void applyUpdateEffect(LivingEntity entity, int amplifier, CallbackInfo callbackInfo) {
+        if (entity.hasStatusEffect(MoreWeaponryEffects.CELESTIAL)) {
+            entity.removeStatusEffect(StatusEffects.REGENERATION);
+        }
+
+    }
+
+}
