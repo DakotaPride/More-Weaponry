6c087ca0f77a65d3bbc792c88d42b0277f53c9d8
DakotaPride <rjtube13@gmail.com>
2022-06-04 18:01:33 -0500

    v1.19.3 Patch

--- src/main/java/net/DakotaPride/moreweaponry/common/MoreWeaponry.java
+++ src/main/java/net/DakotaPride/moreweaponry/common/MoreWeaponry.java
@@ -178,7 +178,7 @@ public class MoreWeaponry implements ModInitializer {
 			.addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "AF8B6E3F-3328-4C0A-AA36-5BA2BB9DBEF3",
 					-1.0D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
 			.addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC",
-					-6.0D, EntityAttributeModifier.Operation.ADDITION);
+					-0.3D, EntityAttributeModifier.Operation.ADDITION);
 	public static final StatusEffect BLEEDING = new BleedingStatusEffect(StatusEffectCategory.HARMFUL, 0xCC4F4F)
 			.addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "AF8B6E3F-3328-4C0A-AA36-5BA2BB9DBEF3",
 					-0.2D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
