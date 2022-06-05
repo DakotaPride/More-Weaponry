4b43ec34517b95b2eba480dad9ced25392607704
DakotaPride <rjtube13@gmail.com>
2022-06-04 22:39:41 -0500

    v1.19.3 Patch

--- src/main/java/net/DakotaPride/moreweaponry/mixin/MobEntityMixin.java
+++ src/main/java/net/DakotaPride/moreweaponry/mixin/MobEntityMixin.java
@@ -3,6 +3,7 @@ package net.DakotaPride.moreweaponry.mixin;
 import net.DakotaPride.moreweaponry.common.MoreWeaponry;
 import net.DakotaPride.moreweaponry.common.entity.damage.MoreWeaponryDamageSource;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.Monster;
 import net.minecraft.entity.mob.SlimeEntity;
@@ -12,9 +13,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SlimeEntity.class)
-public abstract class SlimeEntityMixin extends MobEntity implements Monster {
-    protected SlimeEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
